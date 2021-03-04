6a79064b44d91948a816c447210141f3efd5b2f9
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-03-04 18:52:22 +0100

    Resize screenshot div size

--- src/slides/ds.js
+++ src/slides/ds.js
@@ -4,7 +4,7 @@ import ImageCard from '../components/imageCard';
 export default function WhyCentralizedPlanning() {
     return (
         <section className="bg-primary">
-            <div className="wrap size-80">
+            <div className="wrap size-70">
                 <ImageCard image={ds} imageId="ds-screenshot" />
             </div>
         </section>
