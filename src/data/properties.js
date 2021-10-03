4b682ad3a1c047e7e48059959f94e22eb8133303
Hendrik Bulens <hendrik.bulens@gmail.com>
2021-10-03 15:34:05 -0500

    Swap icons

--- src/data/properties.js
+++ src/data/properties.js
@@ -1,26 +1,27 @@
 import { FormattedMessage } from 'react-intl';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLink, faIndustry, faPlug, faDumbbell } from '@fortawesome/free-solid-svg-icons'
+import flexible from '../static/images/properties/flexible.svg';
+import plugAndPlay from '../static/images/properties/plug-and-play.svg';
+import integrated from '../static/images/properties/integrated.svg';
+import industry from '../static/images/properties/industry.svg';
 
-const iconStyle = { width: "0.55em" };
 export default [
     {
-        icon: <FontAwesomeIcon icon={faLink} style={iconStyle} />,
+        icon: <img src={integrated} />,
         header: <FormattedMessage id="properties.property1.header" defaultMessage='Connected to Microsoft Dynamics 365 Business Central' />,
         text: <FormattedMessage id="properties.property1.text" defaultMessage='Seamless integration between Dime.Scheduler and products from the Microsoft Dynamics suite.' />
     },
     {
-        icon: <FontAwesomeIcon icon={faIndustry} style={iconStyle} />,
+        icon: <img src={industry} style={{ height: "50px" }} />,
         header: <FormattedMessage id="properties.property2.header" defaultMessage='For any type of industry' />,
         text: <FormattedMessage id="properties.property2.text" defaultMessage='Widespread use of Dime.Scheduler in various industries makes it the planning solution of choice for Microsoft Dynamics 365 Business Central.' />
     },
     {
-        icon: <FontAwesomeIcon icon={faDumbbell} style={iconStyle} />,
+        icon: <img src={flexible} />,
         header: <FormattedMessage id="properties.property3.header" defaultMessage='Versatile and flexible' />,
         text: <FormattedMessage id="properties.property3.text" defaultMessage='Tailor Dime.Scheduler to the way your customer works through extensive configuration and customization capabilities.' />
     },
     {
-        icon: <FontAwesomeIcon icon={faPlug} style={iconStyle} />,
+        icon: <img src={plugAndPlay} />,
         header: <FormattedMessage id="properties.property4.header" defaultMessage='Easy to integrate' />,
         text: <FormattedMessage id="properties.property4.text" defaultMessage='Plug and play Dime.Scheduler with your own tools and create a seamless and integrated technological landscape.' />
     }
